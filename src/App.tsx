47f616efb69b5c9bf38d96e1bfe61952eae48b47
lzyjin <1247y.j@gmail.com>
2024-11-01 21:18:51 +0900

    # 8 Framer Motion Animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,151 +1,76 @@
-import {createGlobalStyle, styled} from "styled-components";
-import {DragDropContext, DropResult} from "react-beautiful-dnd";
-import {useRecoilState} from "recoil";
-import {toDoState} from "./atoms";
-import Board from "./components/Board";
+import styled from "styled-components";
+import {motion, AnimatePresence} from "framer-motion";
+import {useState} from "react";
 
-const GlobalStyle = createGlobalStyle`
-  /* http://meyerweb.com/eric/tools/css/reset/
-  v5.0.1 | 20191019
-  License: none (public domain)
-  */
-
-  html, body, div, span, applet, object, iframe,
-  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-  a, abbr, acronym, address, big, cite, code,
-  del, dfn, em, img, ins, kbd, q, s, samp,
-  small, strike, strong, sub, sup, tt, var,
-  b, u, i, center,
-  dl, dt, dd, menu, ol, ul, li,
-  fieldset, form, label, legend,
-  table, caption, tbody, tfoot, thead, tr, th, td,
-  article, aside, canvas, details, embed,
-  figure, figcaption, footer, header, hgroup,
-  main, menu, nav, output, ruby, section, summary,
-  time, mark, audio, video {
-    margin: 0;
-    padding: 0;
-    border: 0;
-    font-size: 100%;
-    font: inherit;
-    vertical-align: baseline;
-  }
-  /* HTML5 display-role reset for older browsers */
-  article, aside, details, figcaption, figure,
-  footer, header, hgroup, main, menu, nav, section {
-    display: block;
-  }
-  /* HTML5 hidden-attribute fix for newer browsers */
-  *[hidden] {
-    display: none;
-  }
-  body {
-    line-height: 1;
-  }
-  menu, ol, ul {
-    list-style: none;
-  }
-  blockquote, q {
-    quotes: none;
-  }
-  blockquote:before, blockquote:after,
-  q:before, q:after {
-    content: '';
-    content: none;
-  }
-  table {
-    border-collapse: collapse;
-    border-spacing: 0;
-  }
-  
-  /* custom styles */
-  body {
-      font-family: "Tinos", serif;
-      background-color: ${props => props.theme.bgColor};
-      color: ${props => props.theme.textColor};
-  }
-  a {
-      text-decoration: none;
-      color: inherit;
-  }
-  * {
-      box-sizing: border-box;
-  }
+const Wrapper = styled(motion.div)`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
-const Wrapper = styled.div`
+const Grid = styled(motion.div)`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    width: 50vw;
+    gap: 20px;
+    
+    div:first-child,
+    div:last-child {
+        grid-column: span 2;
+    }
+`;
+
+const Box = styled(motion.div)`
+  height: 200px;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 40px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  font-size: 1.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Overlay = styled(motion.div)`
+    width: 100%;
+    height: 100%;
+    position: absolute;
     display: flex;
     justify-content: center;
     align-items: center;
-    max-width: 680px;
-    width: 100%;
-    height: 100vh;
-    margin: 0 auto;
-`;
-
-const Boards = styled.div`
-    width: 100%;
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    gap: 10px;
 `;
 
 export default function App() {
-  const [toDos, setToDos] = useRecoilState(toDoState);
-
-  const onDragEnd = (info: DropResult) => {
-    const {destination, source, draggableId} = info;
-
-    if(!destination) {
-      return;
-    }
-
-    if(destination?.droppableId === source.droppableId) {
-      // same board movement.
-      setToDos(allBoards => {
-        const boardCopy = [...allBoards[source.droppableId]];
-        const taskObj = boardCopy[source.index];
-        boardCopy.splice(source.index, 1);
-        boardCopy.splice(destination?.index, 0, taskObj);
-        return {
-          ...allBoards,
-          [source.droppableId]: boardCopy
-        };
-      });
-    }
-
-    if(destination?.droppableId !== source.droppableId) {
-      // cross board movement
-      setToDos(allBoards => {
-        const sourceBoard = [...allBoards[source.droppableId]];
-        const destinationBoard = [...allBoards[destination.droppableId]];
-        const taskObj = sourceBoard[source.index];
-        sourceBoard.splice(source.index, 1);
-        destinationBoard.splice(destination?.index, 0, taskObj);
-        return {
-          ...allBoards,
-          [source.droppableId]: sourceBoard,
-          [destination.droppableId]: destinationBoard,
-        };
-      });
-    }
-
-  };
+  const [id, setId] = useState<string | null>(null);
 
   return (
-    <>
-      <GlobalStyle />
-      <DragDropContext onDragEnd={onDragEnd}>
-        <Wrapper>
-          <Boards>
-            {
-              Object.keys(toDos).map(boardId => (
-                <Board key={boardId} boardId={boardId} toDos={toDos[boardId]}/>
-              ))
-            }
-          </Boards>
-        </Wrapper>
-      </DragDropContext>
-    </>
+    <Wrapper>
+      <Grid>
+        {
+          ["1", "2", "3", "4"].map(n => (
+            <Box key={n} layoutId={n} onClick={() => setId(n)} />
+          ))
+        }
+      </Grid>
+      <AnimatePresence>
+        {
+          id
+          ?
+          <Overlay
+            onClick={() => setId(null)}
+            initial={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
+            animate={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
+            exit={{ backgroundColor: "rgba(0, 0, 0, 0)" }} >
+            <Box
+              layoutId={id}
+              style={{ width: 400, height: 200 }} />
+          </Overlay>
+          :
+          null
+        }
+      </AnimatePresence>
+    </Wrapper>
   );
 }
